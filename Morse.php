a42d9fbe37ad4830b7fbf2dc867361df80f4ef97
chribango <166618337+chribango@users.noreply.github.com>
2025-10-01 11:20:47 +0200

    Update Morse.php

--- Morse.php
+++ Morse.php
@@ -3,8 +3,10 @@
 <form action="Codice_Morse"method="post">
     Messaggio da criptare:
     <input type="text" id="messaggio" name="messaggio"><br>
-    <button type="button"> Codifica</button>
+    <button type="button"> Codifica</button><br>
+    <input type="text" id="messaggio" name="messaggio2" disabled><br>
 </form>
+
 </body>
 <?php
 $Codifica=array(
@@ -14,12 +16,23 @@ $Codifica=array(
     'P' => '.--.',  'Q' => '--.-',  'R' => '.-.',   'S' => '...',   'T' => '-',
     'U' => '..-',   'V' => '...-',  'W' => '.--',   'X' => '-..-',  'Y' => '-.--',
     'Z' => '--..',
+    'a' => '.-',    'b' => '-...',  'c' => '-.-.',  'd' => '-..',   'e' => '.',
+    'f' => '..-.',  'g' => '--.',   'h' => '....',  'i' => '..',    'j' => '.---',
+    'k' => '-.-',   'l' => '.-..',  'm' => '--',    'n' => '-.',    'o' => '---',
+    'p' => '.--.',  'q' => '--.-',  'r' => '.-.',   's' => '...',   't' => '-',
+    'u' => '..-',   'v' => '...-',  'w' => '.--',   'x' => '-..-',  'y' => '-.--',
+    'z' => '--..',
     '0' => '-----', '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-',
     '5' => '.....', '6' => '-....', '7' => '--...', '8' => '---..', '9' => '----.',
     '.' => '.-.-.-', ',' => '--..--', '?' => '..--..', '/' => '-..-.', '-' => '-....-',
     '(' => '-.--.', ')' => '-.--.-', ' ' => '/', // Usiamo '/' per lo spazio, una convenzione comune
     );
 $Messaggio[]=$_POST['Messaggio'];
+$Elaborato=str_split($_POST['Messaggio']);
+$dim=count($Elaborato);
+for($i=0; $i<$dim; $i++){
+    if($Elaborato[$i]===
+}
 
 ?>
 
